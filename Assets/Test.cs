4f5aeacb6dc853c3c53735978837150cb41b9d73
rei0ier <139188412+rei0ier@users.noreply.github.com>
2023-09-14 10:38:29 +0900

    Bossクラス,mpとMagic関数
    Lesson4発展課題

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,25 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int mp = 53; //マジックポイント
+
+    //魔法攻撃関数
+    public void Magic(int loss)
+    {
+        if (mp >= loss)
+        {
+            mp -= loss;
+            Debug.Log("魔法攻撃をした。残りMPは"+ mp +"。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない");
+        }
+    }
+
+}
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //配列を初期化する
-        int[] array = { 80, 30, 70, 100, 20 };
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss Mboss = new Boss();
 
-        //要素を全て表示
-        for(int i = 0; i < 5; i++)
+        //Magic関数を10回使う
+        for (int i = 0; i < 10; i++)
         {
-            Debug.Log(array[i]);
+            Mboss.Magic(5);
         }
 
-        //要素を逆順で表示する
-        for(int i = 4; i >= 0; i--)
-        {
-            Debug.Log(array[i]);
-        }
+        //さらにMagic関数を呼び出す
+        Mboss.Magic(5);
     }
 
     // Update is called once per frame
