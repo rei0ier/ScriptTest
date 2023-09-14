f1a04cb98ab66273c98852e3718db440569afc20
rei0ier <139188412+rei0ier@users.noreply.github.com>
2023-09-14 09:34:57 +0900

    再課題

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,32 +4,25 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
-        // 要素数5の配列を初期化する
-        int[] array = new int[5];
+        //配列を初期化する
+        int[] array = { 80, 30, 70, 100, 20 };
 
-        //配列の各要素に値を値を代入する
-        array[0] = 10;
-        array[1] = 90;
-        array[2] = 30;
-        array[3] = 20;
-        array[4] = 40;
-
-        //配列の要素全てを表示する
+        //要素を全て表示
         for(int i = 0; i < 5; i++)
         {
             Debug.Log(array[i]);
         }
 
-        //配列を逆順で表示する
-        for (int i = 4; i >= 0; i--)
+        //要素を逆順で表示する
+        for(int i = 4; i >= 0; i--)
         {
             Debug.Log(array[i]);
         }
     }
+
     // Update is called once per frame
     void Update()
     {
